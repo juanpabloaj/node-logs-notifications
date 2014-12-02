81a959dcf80a12013d420b94d34331194f0eba83
JuanPablo <jpabloaj@gmail.com>
2014-12-02 20:35:17 +0000

    split long line

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var Slack = require('slack-node');
 function log(message) {
   if ( process.env.SLACK_TOKEN !== undefined ) {
     slack = new Slack(process.env.SLACK_TOKEN);
-    slack.api('chat.postMessage', {text:message, channel:'#logs'}, function(){});
+    slack.api(
+      'chat.postMessage',
+      {text:message, channel:'#logs'},
+      function(){}
+    );
   } else {
     console.log(message);
   }
